9d6cd3b800eba3ced2ed3ea75a057b46e124cb6b
pgdien <pgdien2@gmail.com>
2016-11-15 16:46:49 +0700

    Add loading
    Add loading
    Thêm kích thước ảnh

--- CMS/obj/Release/Package/PackageTmp/Scripts/front/header-footer.js
+++ CMS/obj/Release/Package/PackageTmp/Scripts/front/header-footer.js
@@ -1,10 +1,8 @@
 ﻿/*Nút search*/
-
 $("#button").click(function () {
     $("#search-box").show();
     $('#button').hide();
 });
-
 $(document).click(function (e) {
     if (!$(e.target).hasClass("button")
         && $(e.target).parents("#search-box").length === 0) {
@@ -13,3 +11,18 @@ $(document).click(function (e) {
     }
 });
 
+/*Nút tìm kiếm trên di động*/
+$("#search-btn").click(function () {
+    console.log($("#search-box-mobile").is(":visible"));
+    if (!$("#search-box-mobile").is(":visible")) {
+        $("#search-box-mobile").slideToggle("medium");
+        $("#dl-menu").css("border-bottom", "none");
+    } else {
+        $("#search-box-mobile").slideToggle("medium");
+    }
+});
+$(document).click(function (e) {
+    if (!$(e.target).hasClass("search-btn") && !$(e.target).hasClass("search-box-mobile")) {
+        $("#search-box-mobile").slideUp("Medium");
+    }
+});
